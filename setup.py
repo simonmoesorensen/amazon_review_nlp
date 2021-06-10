4b1b94c0625bd080d5c7a897d930440928ef75be
simonmoesorensen <moe.simon@gmail.com>
2021-06-10 11:49:24 +0200

    Using cookiecutter datascience template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict sentiment on amazon reviews using the huggingface/transformers package with testing, CI/CD and cloud-computing',
+    author='Simon Moe Sørensen',
+    license='MIT',
+)
