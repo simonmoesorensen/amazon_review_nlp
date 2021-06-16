caba4f183220fa03494dd4e67805f0cce0a1c8ce
Silas Brack <s174433@student.dtu.dk>
2021-06-16 17:56:38 +0200

    Removed unnecessary method

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -6,21 +6,6 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
-import torch
-
-
-def collate_fn(batch):
-    labels = torch.stack([x["labels"] for x in batch], axis=0)
-    input_ids = torch.stack([x["input_ids"] for x in batch], axis=0)
-    token_type_ids = torch.stack([x["token_type_ids"] for x in batch], axis=0)
-    attention_mask = torch.stack([x["attention_mask"] for x in batch], axis=0)
-    return {
-        "labels": labels,
-        "input_ids": input_ids,
-        "token_type_ids": token_type_ids,
-        "attention_mask": attention_mask,
-    }
-
 
 model_name = "distilbert-base-cased"
 model = DistilBertSentimentClassifier(model_name)
@@ -28,13 +13,10 @@ tokenizer = DistilBertTokenizer.from_pretrained(model_name)
 
 data = AmazonReviewFullDataModule(tokenizer)
 
-labels, x = next(iter(data.train_dataloader()))
-out = model(**x)
-
 trainer_params = {
     "gpus": 0,
     "max_epochs": 3,
-    # "precision": 16,
+    # "precision": 16,  # only on GPU
     "progress_bar_refresh_rate": 20,
     "log_every_n_steps": 10,
     "callbacks": [
